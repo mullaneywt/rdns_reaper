64e0d215f0c9ebdae4968f77359c54f5f2094722
mullaneywt <will.mullaney@gmail.com>
2022-07-12 18:35:49 +0000

    Added initial pytest and updated requirements

---
+++ rdns_reaper/test/test_rdns_reaper.py
@@ -0,0 +1,11 @@
+import pytest
+from rdns_reaper import rdns_reaper
+
+def test_simpletest_1():
+    dns = rdns_reaper()
+    assert isinstance(dns, rdns_reaper)
+
+def test_simple_iadd():
+    dns = rdns_reaper()
+    dns += "10.0.0.1"
+    assert dns["10.0.0.1"] == None
