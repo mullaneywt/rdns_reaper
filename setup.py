8828e882305c1a3d29eaad0075018bdadc4254c4
mullaneywt <will.mullaney@gmail.com>
2022-07-11 23:34:28 -0400

    Initial Commit and changes to .gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='rdns_reaper',
+    version='0.0.1',
+    author='Will Mullaney',
+    author_email='will.mullaney@gmail.com',
+    description='Reverse DNS lookup engine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mullaneywt/rdns_reaper',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mullaneywt/rdns_reaper/issues"
+    },
+    license='None',
+    packages=['rdns_reaper'],
+    install_requires=['netaddr', 'pyyaml'],
+)
