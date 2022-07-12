7e969844d3dac2c5b6683e67e4c0fbd08e9cd174
mullaneywt <will.mullaney@gmail.com>
2022-07-11 23:51:40 -0400

    Fix issue with match-case statements, add initial test

--- rdns_reaper/rdns_reaper.py
+++ rdns_reaper/rdns_reaper.py
@@ -12,18 +12,13 @@ class rdns_reaper:
         self._resolver_ip = None
 
         if "limit_to_rfc1918" in kwargs.keys():
-            match kwargs["limit_to_rfc1918"]:
-                case bool():
-                    self._limit_to_rfc1918 = kwargs["limit_to_rfc1918"]
-                case _:
-                    #User passed an invalid response
-                    self._limit_to_rfc1918 = False
+            if isinstance(kwargs["limit_to_rfc1918"], bool):
+                self._limit_to_rfc1918 = kwargs["limit_to_rfc1918"]
+            else:
+                # User passed an invalid response
+                self._limit_to_rfc1918 = False
         else:
             self._limit_to_rfc1918 = False
-
-
-
-
 
         """Process parallel lookup concurrency"""
         try:
@@ -33,11 +28,6 @@ class rdns_reaper:
                 raise TypeError
         except KeyError:
             self._concurrent = 1
-
-        
-
-
-
 
         """Determine how to mark unresolvable entries"""
         try:
@@ -310,56 +300,3 @@ class rdns_reaper:
             else:
                 raise StopIteration
             pass
-
-
-def main():
-    """Test functionality."""
-    dr = rdns_reaper(limit_to_rfc1918=True, concurrent=20, unresolvable=r"N\A")
-    dr2 = rdns_reaper(limit_to_rfc1918=True, concurrent=20, unresolvable=r"N\A")
-    # dr.set_resolver("10.100.1.88")
-    try:
-        dr.loadfile("dns_test.yaml")
-    except FileNotFoundError:
-        pass
-
-    # # dr.add_ip_list(["8.8.8.8", "1.1.1.1", "8.8.4.4", "10.100.1.88"])
-    # dr.add_ip_list(["1.1.1.1", "10.100.1.88", "10.100.1.99"])
-    # dr2.add_ip_list(["8.8.8.8", "8.8.4.4"])
-
-    # dr += dr2
-    # dr += "1.1.1.2"
-    # dr += ["1.1.1.3", "1.1.1.4"]
-    # # print(dr.__dict__)
-    # dr.resolve_all()
-    # # print("1.1.1.1" in dr)
-    # # print(dr["1.1.1.1"])
-    # # print("1.1.1.2" in dr)
-    # # print(dr["1.1.1.2"])
-    # dr.savefile("dns_test.yaml")
-
-
-    d1 = rdns_reaper(
-        limit_to_rfc1918=None,
-        concurrent=20,
-        unresolvable=r"N\A",
-        filename="d1.yaml",
-        filemode="w",
-    )
-
-
-    d1 += "1.1.1.1"
-    d1 += "1.1.1.2"
-
-    d1.resolve_all()
-    d1.savefile()
-
-    # close(d1)
-
-    print(d1.__dict__)
-    # for k, v in d1:
-    #    print(f"{k} - {v}")
-
-
-if __name__ == "__main__":
-    # main()
-    pass
---
+++ rdns_reaper/test/basetest.py
@@ -0,0 +1,51 @@
+from rdns_reaper import rdns_reaper
+
+def main():
+    """Test functionality."""
+    dr = rdns_reaper(limit_to_rfc1918=True, concurrent=20, unresolvable=r"N\A")
+    dr2 = rdns_reaper(limit_to_rfc1918=True, concurrent=20, unresolvable=r"N\A")
+    # dr.set_resolver("10.100.1.88")
+    try:
+        dr.loadfile("dns_test.yaml")
+    except FileNotFoundError:
+        pass
+
+    # # dr.add_ip_list(["8.8.8.8", "1.1.1.1", "8.8.4.4", "10.100.1.88"])
+    # dr.add_ip_list(["1.1.1.1", "10.100.1.88", "10.100.1.99"])
+    # dr2.add_ip_list(["8.8.8.8", "8.8.4.4"])
+
+    # dr += dr2
+    # dr += "1.1.1.2"
+    # dr += ["1.1.1.3", "1.1.1.4"]
+    # # print(dr.__dict__)
+    # dr.resolve_all()
+    # # print("1.1.1.1" in dr)
+    # # print(dr["1.1.1.1"])
+    # # print("1.1.1.2" in dr)
+    # # print(dr["1.1.1.2"])
+    # dr.savefile("dns_test.yaml")
+
+    d1 = rdns_reaper(
+        limit_to_rfc1918=None,
+        concurrent=20,
+        unresolvable=r"N\A",
+        filename="d1.yaml",
+        filemode="w",
+    )
+
+    d1 += "1.1.1.1"
+    d1 += "1.1.1.2"
+
+    d1.resolve_all()
+    d1.savefile()
+
+    # close(d1)
+
+    print(d1.__dict__)
+    # for k, v in d1:
+    #    print(f"{k} - {v}")
+
+
+if __name__ == "__main__":
+    main()
+    pass
