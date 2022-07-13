ebeede9e7da77e4f4ab9555c1aabd41a4c48f657
mullaneywt <will.mullaney@gmail.com>
2022-07-13 00:33:55 -0400

    Ver 0.0.8

--- setup.py
+++ setup.py
@@ -6,17 +6,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='rdns_reaper',
-    version='0.0.7',
+    version='0.0.8',
     author='Will Mullaney',
     author_email='rdns-reaper@mullaneywt.anonaddy.com',
     description='Reverse DNS lookup engine',
+    homepage='https://github.com/mullaneywt/rdns_reaper',
+    install_requires=['netaddr>=0.8.0', 'pyyaml>=6.0'],
+    keywords=['reverse', 'dns'],
+    license='None',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mullaneywt/rdns_reaper',
-    project_urls = {
+    packages=['rdns_reaper'],
+    python_requires='>=3.6,<=3.11',
+    project_urls={
         "Bug Tracker": "https://github.com/mullaneywt/rdns_reaper/issues"
     },
-    license='None',
-    packages=['rdns_reaper'],
-    install_requires=['netaddr', 'pyyaml'],
+    url='https://github.com/mullaneywt/rdns_reaper'
 )
