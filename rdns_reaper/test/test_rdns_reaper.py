0e834685c21239cb3f5d02df3ebb3f392a928e31
mullaneywt <will.mullaney@gmail.com>
2022-07-13 03:38:15 +0000

    Updated automated unit testing and tox

--- rdns_reaper/test/test_rdns_reaper.py
+++ rdns_reaper/test/test_rdns_reaper.py
@@ -11,3 +11,62 @@ def test_simple_iadd():
     dns = rdns_reaper()
     dns += "10.0.0.1"
     assert dns["10.0.0.1"] == None
+
+
+def test__add__():
+    dns1 = rdns_reaper()
+    dns2 = rdns_reaper()
+    dns1 += "10.0.0.1"
+    dns2 += "10.0.0.2"
+    dns3 = dns1 + dns2
+    assert dns3.keys() == ["10.0.0.1", "10.0.0.2"]
+
+
+def test__contains():
+    dns1 = rdns_reaper()
+    dns1 += "10.0.0.1"
+    assert "10.0.0.1" in dns1
+
+
+def test__del__():
+    dns1 = rdns_reaper()
+    dns1 += "10.0.0.1"
+    del dns1["10.0.0.1"]
+    assert "10.0.0.1" not in dns1
+
+
+def test_add_ip():
+    dns1 = rdns_reaper()
+    dns1.add_ip("10.0.0.1")
+    assert "10.0.0.1" in dns1
+
+
+def test_add_ip_list():
+    dns1 = rdns_reaper()
+    dns1.add_ip_list(["10.0.0.1"])
+    assert "10.0.0.1" in dns1
+
+
+def test_limit_to_rfc1918_true():
+    dns1 = rdns_reaper()
+    dns1.limit_to_rfc1918(True)
+    assert dns1._limit_to_rfc1918
+
+
+def test_limit_to_rfc1918_false():
+    dns1 = rdns_reaper()
+    dns1.limit_to_rfc1918(False)
+    assert dns1._limit_to_rfc1918 is False
+
+
+def test_iterator():
+    dns = rdns_reaper()
+    dns.add_ip_list(["1.1.1.1", "8.8.8.8"])
+    dns.resolve_all_serial()
+    l1 = [("1.1.1.1", "one.one.one.one"), ("8.8.8.8", "dns.google")]
+
+    for address in dns:
+        if address not in l1:
+            assert False
+
+    assert True
