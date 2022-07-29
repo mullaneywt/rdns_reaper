fb57381ed2e1809188890790e84b2f27d346cf4c
mullaneywt <50823494+mullaneywt@users.noreply.github.com>
2022-07-28 23:45:50 -0600

    Sphinx and RTFD is broken due to a problem with type hints

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../rdns_reaper"))
-import rdns_reaper
+# sys.path.insert(0, os.path.abspath("../rdns_reaper"))
+# import rdns_reaper
 
 
 # -- Project information -----------------------------------------------------
